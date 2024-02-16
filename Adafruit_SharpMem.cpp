c971536cc7e0264e8895efe9f2fea4cd4fc43361
tim knapen <timknapen@gmail.com>
2024-02-17 00:30:40 +0100

    faster 1bit rectangle filling

--- Adafruit_SharpMem.cpp
+++ Adafruit_SharpMem.cpp
@@ -383,6 +383,15 @@ void Adafruit_SharpMem::setBitmap(uint8_t *bitmap) {
   memcpy(sharpmem_buffer, bitmap, (WIDTH * HEIGHT) / 8);
 }
 
+
+/**************************************************************************/
+void Adafruit_SharpMem::fillRect(int16_t x, int16_t y, int16_t w, int16_t h,
+                            uint16_t color) {
+  for (int16_t i = y; i < y + h; i++) {
+    drawFastHLine(x, i, w, color);
+  }
+}
+
 /**************************************************************************/
 void Adafruit_SharpMem::drawFastHLine(int16_t x, int16_t y, int16_t w,
                                       uint16_t color) {
